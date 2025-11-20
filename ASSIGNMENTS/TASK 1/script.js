774ef07f11af8062906f34ca55d265a722388064
vandana-pathak12 <vandanapathak1212@gmail.com>
2025-11-20 22:50:40 +0530

    do

--- ASSIGNMENTS/TASK 1/script.js
+++ ASSIGNMENTS/TASK 1/script.js
@@ -1,47 +1,25 @@
-let form = document.getElementById("myForm");
-let cards = document.getElementById("cards");
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
 
-    let photo = document.getElementById("photo").files[0];
-    let name = document.getElementById("name").value;
-    let role = document.getElementById("role").value;
-    let info = document.getElementById("info").value;
+ function addTask() {
+            let taskText = document.getElementById("taskInput").value;
 
-    if (!photo || name === "" || role === "" || info === "") {
-        alert("Please fill all fields.");
-        return;
-    }
+            if (taskText === "") {
+                alert("enter");
+                return;
+            }
 
-    let reader = new FileReader();
+            let li = document.createElement("li");
+            li.textContent = taskText;
+            let delbtn = document.createElement("button");
+            delbtn.textContent = "Delete";
+            delbtn.onclick = function() {
+                li.remove();
+            }
 
-    reader.onload = function (event) {
-        let card = document.createElement("div");
-        card.classList.add("card");
-        let profile = document.createElement("div");
-        profile.classList.add("profile");
-        let img = document.createElement("img");
-        img.src = event.target.result;
-        let h2 = document.createElement("h2");
-        h2.textContent = name;
-        let h3 = document.createElement("h3");
-        h3.textContent = role;
-        let p = document.createElement("p");
-        p.textContent = info;
+            li.appendChild(delbtn);
 
-        profile.appendChild(img);
-        card.appendChild(profile);
-        card.appendChild(h2);
-        card.appendChild(h3);
-        card.appendChild(p);
+            document.getElementById("taskList").appendChild(li);
 
-        cards.appendChild(card);
-
-        form.reset();
-    }
-
-    reader.readAsDataURL(photo);
-});
-
+            document.getElementById("taskInput").value = "";
+        }
 
 
